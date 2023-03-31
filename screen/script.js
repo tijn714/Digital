13ae327d21acaa2ab96abd8541a9bb7fe158f99d
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2023-03-31 10:49:33 -0400

    Add files via upload

--- screen/script.js
+++ screen/script.js
@@ -10,7 +10,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 7500); // Change image every 5 seconds
 }
 
 
